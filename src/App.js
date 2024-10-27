2e460fa3e3da3d0cfca6723d094ec65bda85f30c
miakirei <kurobaneamaya@example.com>
2024-10-28 06:21:06 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -32,22 +32,22 @@ function App() {
         <Routes location={background || location}>
           <Route path="/flavors" element={<Layout />}>
             <Route index element={<Home db={spices.concat(mixes, chilies, peppers, herbs)} cat={categories} />} />
-            <Route path="products" element={<Products db={spices.concat(mixes, chilies, peppers, herbs)} />} />
-            <Route path="products/:id" element={<ProductInfo db={spices.concat(mixes, chilies, peppers, herbs)} />} />
-            <Route path="categories" element={<Categories cat={categories} />} />
-            <Route path="categories/:id" element={<CategoryInfo db={spices.concat(mixes, chilies, peppers, herbs)} cat={categories} />} />
-            <Route path="gallery" element={<Gallery db={gallery} />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="sitemap" element={<SiteMap db={spices.concat(mixes, chilies, peppers, herbs)} cat={categories} />} />
-            <Route path="search" element={<Search db={spices.concat(mixes, chilies, peppers, herbs)} />} />
+            <Route path="/flavors/products" element={<Products db={spices.concat(mixes, chilies, peppers, herbs)} />} />
+            <Route path="/flavors/products/:id" element={<ProductInfo db={spices.concat(mixes, chilies, peppers, herbs)} />} />
+            <Route path="/flavors/categories" element={<Categories cat={categories} />} />
+            <Route path="/flavors/categories/:id" element={<CategoryInfo db={spices.concat(mixes, chilies, peppers, herbs)} cat={categories} />} />
+            <Route path="/flavors/gallery" element={<Gallery db={gallery} />} />
+            <Route path="/flavors/about" element={<About />} />
+            <Route path="/flavors/contact" element={<Contact />} />
+            <Route path="/flavors/sitemap" element={<SiteMap db={spices.concat(mixes, chilies, peppers, herbs)} cat={categories} />} />
+            <Route path="/flavors/search" element={<Search db={spices.concat(mixes, chilies, peppers, herbs)} />} />
           </Route>
         </Routes>
 
         {/* Show the modal when a background page is set */}
         {background && (
           <Routes>
-            <Route path="products/:id" element={<ProductModal db={spices.concat(mixes, chilies, peppers, herbs)} />} />
+            <Route path="/flavors/products/:id" element={<ProductModal db={spices.concat(mixes, chilies, peppers, herbs)} />} />
           </Routes>
         )}
       </div>
